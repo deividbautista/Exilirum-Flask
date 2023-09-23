b4ef65dd005bce7003e360d4dfaa2567b601b918
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-09-23 20:10:26 +0100

    Removing Code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Project Exilirum
+
+## Requirements
+* [Python](https://www.python.org/downloads/release/python-360/)
+* [VirtualEnv](https://pypi.org/project/virtualenv/)
--- app/app.py
+++ app/app.py
@@ -2,25 +2,23 @@
 # Importamos la librería de render_template para poder renderizar los templates
 from flask import Flask, render_template
 
-#-------------------------IMPORTS BOOKSTORES---------------------#
+# -------------------------IMPORTS BOOKSTORES---------------------#
 
 
-
-#-------------------------CREATE APP---------------------#
+# -------------------------CREATE APP---------------------#
 # Creamos una instancia de Flask
 app = Flask(__name__)
 
+
 # Definimos la ruta principal con el archivo home.html
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('pages/home.html')
+    return render_template("pages/home.html")
 
 
-
-#-------------------------Ejecutando la aplicación---------------------#
+# -------------------------Ejecutando la aplicación---------------------#
 # Ejecutamos la aplicación
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # Activamos el modo debug para que se recargue la página
     app.debug = True
 
